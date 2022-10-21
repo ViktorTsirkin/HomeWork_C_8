8a0fb2698de13079070ec983cd97c264a206e2f9
Viktor Tsirkin <reset1985@mail.ru>
2022-10-22 02:25:25 +0300

    add solution task56

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -22,48 +22,48 @@ int minSumLine = 0;
 int sumLine = SumLineElements(array, 0);
 for (int i = 1; i < array.GetLength(0); i++)
 {
-  int tempSumLine = SumLineElements(array, i);
-  if (sumLine > tempSumLine)
-  {
-    sumLine = tempSumLine;
-    minSumLine = i;
-  }
+    int tempSumLine = SumLineElements(array, i);
+    if (sumLine > tempSumLine)
+    {
+        sumLine = tempSumLine;
+        minSumLine = i;
+    }
 }
 
-Console.WriteLine($"\nСтрокa с наименьшей суммой элементов - {minSumLine+1}. Сумма элементов в ней равна {sumLine}"); // {minSumLine+1} - чтобы нулевая строка считаласт первой, первая - второй и т.д.
+Console.WriteLine($"\nСтрокa с наименьшей суммой элементов - {minSumLine + 1}. Сумма элементов в ней равна {sumLine}"); // {minSumLine+1} - чтобы нулевая строка считаласт первой, первая - второй и т.д.
 
 
 int SumLineElements(int[,] array, int i)
 {
-  int sumLine = array[i,0];
-  for (int j = 1; j < array.GetLength(1); j++)
-  {
-    sumLine += array[i,j];
-  }
-  return sumLine;
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
 }
 
 
-int[,] GetArray(int m, int n, int minValue, int maxValue) 
+int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
     int[,] result = new int[m, n];
-    for(int i = 0; i < m; i++) 
+    for (int i = 0; i < m; i++)
     {
-        for(int j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)
         {
-            result[i,j] = new Random().Next(minValue, maxValue + 1);
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
         }
     }
     return result;
 }
 
-void PrintArray(int[,] array) 
+void PrintArray(int[,] array)
 {
-    for(int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for(int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"{array[i,j]} ");
+            Console.Write($"{array[i, j]} ");
         }
         Console.WriteLine();
     }
